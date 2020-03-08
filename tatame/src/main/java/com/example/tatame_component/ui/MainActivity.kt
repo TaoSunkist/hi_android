3767ac55a1254942adc460848a9022524c914d7a
thsunkist@gmail.com <thsunkist@gmail.com>
2020-03-08 20:32:47 +0800

    [fix]fix gradle dependencies

---
+++ tatame/src/main/java/com/example/tatame_component/ui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tatame_component.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
