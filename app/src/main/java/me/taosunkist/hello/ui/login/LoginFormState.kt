51b14d75b55ea5ee6ba8df72320a970a1d2a6669
TaoSunkist <thsunkist@gmail.com>
2021-12-09 14:58:44 +0800

    [fix]fix master

---
+++ app/src/main/java/me/taosunkist/hello/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package me.taosunkist.hello.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false,
+)
