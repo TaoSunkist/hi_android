a08e8ab17149445a0e9ccc75c9850abdfd860446
TaoSunkist <thsunkist@gmail.com>
2021-12-31 17:02:16 +0800

    [fix]fix some bugs and append some feature

---
+++ app/src/main/java/me/taosunkist/hello/ui/animations/SvgaItemListFragment.kt
@@ -0,0 +1,18 @@
+package me.taosunkist.hello.ui.animations
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.taosunkist.hello.R
+
+class SvgaItemListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_lottie, container, false)
+    }
+}
