e5c22c6ceb0b3fe6b7903eeecc300ffe36958cc7
TaoSunkist <thsunkist@gmail.com>
2022-01-25 18:51:15 +0800

    [add]add some gson

--- app/src/main/java/me/taosunkist/hello/data/net/ServerConfigure.kt
+++ app/src/main/java/me/taosunkist/hello/data/net/ServerConfigure.kt
@@ -16,20 +16,20 @@ val availableServers = mapOf(
         name = "Production",
         description = " N/A",
         key = kProductionServer,
-        baseUrl = "http://127.0.0.1:8080",
+        baseUrl = "http://192.168.0.140:8080",
     ),
 
     kTestServer to ServerInfo(
         name = "Test",
         description = " N/A",
         key = kTestServer,
-        baseUrl = "http://127.0.0.1:8080",
+        baseUrl = "http://192.168.0.140:8080",
     ),
 
     kLocalServer to ServerInfo(
         name = "Local",
         description = " N/A",
         key = kLocalServer,
-        baseUrl = "http://127.0.0.1:8080",
+        baseUrl = "http://192.168.0.140:8080",
     )
 )
