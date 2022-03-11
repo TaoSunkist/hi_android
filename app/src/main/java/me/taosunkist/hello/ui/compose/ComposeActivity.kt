5a6a42cd87dc9fe909bbd3ce7fd8773bf6d89e35
TaoSunkist <thsunkist@gmail.com>
2022-03-11 15:26:15 +0800

    [fix]fix some

---
+++ app/src/main/java/me/taosunkist/hello/ui/compose/ComposeActivity.kt
@@ -0,0 +1,40 @@
+package me.taosunkist.hello.ui.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import me.taosunkist.hello.ui.compose.ui.theme.Hi_androidTheme
+
+class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Hi_androidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Hi_androidTheme {
+        Greeting("Android")
+    }
+}
